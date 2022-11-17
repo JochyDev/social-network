105867da99495f2b19b8e7919f7bd5ee364b96d3
JochyDev <joseluisjimenez1998@gmail.com>
2022-11-17 14:15:12 -0500

    add microservice post

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ const swaggerUi = require('swagger-ui-express');
 const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
-const post = require('./components/post/network');
 const errors = require('../network/error');
 
 
@@ -19,7 +18,6 @@ app.use(express.json())
 // Routes
 app.use('/api/user', user);
 app.use('/api/auth', auth);
-app.use('/api/post', post);
 
 // API-DOCS: Swagger
 const swaggerDoc = require('./swagger.json');
